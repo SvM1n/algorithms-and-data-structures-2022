88640645eb4bce6db472f787cab9e64542f6fda2
zonky <Juphy@yandex.ru>
2022-12-04 14:37:19 +0300

    Добавлена 30 практическая работа

---
+++ students/21K0602/21K0602-p30/src/main/java/ru/mirea/practice/task2/Tree.java
@@ -0,0 +1,55 @@
+package ru.mirea.practice.task2;
+
+
+import org.jetbrains.annotations.NotNull;
+
+public class Tree implements Comparable<Tree> {
+
+    int weight;
+    Character content;
+    Tree left;
+    Tree right;
+
+    public Tree(int weight, Character content) {
+        this.weight = weight;
+        this.content = content;
+    }
+
+    public Tree(int weight, Character content, Tree left, Tree right) {
+        this.weight = weight;
+        this.content = content;
+        this.left = left;
+        this.right = right;
+    }
+
+
+    @Override
+    public int compareTo(@NotNull Tree o) {
+        return o.weight - weight;
+    }
+
+    //обход дерева в глубину. Для всех листов которые нам не нужны будет возвращено значение null
+    // и только для нужного вернется код. ( Рекурсивный обход)
+    public String getCodeForCharacter(Character c, String parentPath) {
+        if (content != null && content.equals(c)) {
+            return parentPath;
+        } else {
+            if (left != null) {
+                String path = left.getCodeForCharacter(c, parentPath + 0);
+                if (path != null) {
+                    return path;
+                }
+            }
+            if (right != null) {
+                String path = right.getCodeForCharacter(c, parentPath + 1);
+                if (path != null) {
+                    return path;
+                }
+            }
+        }
+        return null;
+    }
+
+
+}
+

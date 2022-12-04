cfa103e17a2f283f5245728202c9ec2b00bf7543
Норден <kirilleven@mail.ru>
2022-12-04 22:56:34 +0300

    Practice1

---
+++ students/21K0613/21K0613-p01/src/ru/mirea/practice/Task3.java
@@ -0,0 +1,29 @@
+package ru.mirea.practice;
+
+import java.util.Scanner;
+
+abstract class Task3 {
+    public static void main(String[] args) {
+        int sum = 0;
+        System.out.println("Введите количество чисел\t");
+
+        Scanner scn = new Scanner(System.in);
+        int numCount = 0;
+        if (scn.hasNextInt()){
+            numCount = scn.nextInt();
+        }
+        int[] array = new int[numCount];
+
+        System.out.println("Начинайте вводить числа:\t");
+        for (int i = 0; i < numCount; i++){
+            int num = 0;
+            if (scn.hasNextInt()){
+                num = scn.nextInt();
+            }
+            array[i] = num;
+            sum += num;
+        }
+
+        System.out.println("Сумма чисел\t" + sum + "\n" + "Среднее арифметическое\t" + String.valueOf(sum/numCount));
+    }
+}

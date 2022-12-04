94878571c98e8b87cc0876c8b0cabd29640b380e
Норден <kirilleven@mail.ru>
2022-12-05 02:22:29 +0300

    Practice1

---
+++ students/21K0613/21K0613-p01/src/main/java/ru/mirea/practice/s21K0613/Task5.java
@@ -0,0 +1,10 @@
+package ru.mirea.practice.s21K0613;
+
+abstract class Task5 {
+    public static void main(String[] args){
+        for(String str : args){
+            System.out.println(str);
+        }
+    }
+}
+

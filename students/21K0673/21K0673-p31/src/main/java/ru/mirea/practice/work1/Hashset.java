8719ec17e1a80f03754eb374e5a3407a0af2f2ac
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-12-06 22:22:40 +0300

    31 практика

--- students/21K0673/21K0673-p31/src/main/java/ru/mirea/practice/work1/Hashset.java
+++ students/21K0673/21K0673-p31/src/main/java/ru/mirea/practice/work1/Hashset.java
@@ -1,17 +1,18 @@
 package ru.mirea.practice.work1;
 
-import java.util.HashSet;
-import java.util.TreeSet;
+import java.io.FileReader;
+import java.io.IOException;
 
 abstract class Hashset {
     public static void main(String[] args) {
-        HashSet<String> set = new HashSet<>();
-        set.add("word");
-        set.add("not");
-        set.add("agh");
-        set.add("reh");
-        System.out.println(set);
-        TreeSet<String> treeset = new TreeSet<>(set);
-        System.out.println(treeset);
+        try (FileReader reader = new FileReader("C:\\Users\\ProPc\\IdeaProjects\\algorithm-forkedd\\students"
+                + "\\21K0673\\21K0673-p31\\src\\main\\java\\ru\\mirea\\practice\\work1\\PROCS.txt")) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                System.out.print((char) c);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }

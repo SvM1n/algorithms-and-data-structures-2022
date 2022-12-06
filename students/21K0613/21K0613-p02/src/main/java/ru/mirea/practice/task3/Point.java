f31e7571b876a2812e78d9e01eb3106829165a72
Норден <54271099+nordenfall@users.noreply.github.com>
2022-12-07 01:39:43 +0300

    task2
    ball + testball
    (+ task3 beggining)

---
+++ students/21K0613/21K0613-p02/src/main/java/ru/mirea/practice/task3/Point.java
@@ -0,0 +1,11 @@
+package ru.mirea.practice.task3;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(){
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+}

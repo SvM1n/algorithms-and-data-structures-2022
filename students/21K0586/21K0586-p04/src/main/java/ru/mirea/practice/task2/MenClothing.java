6862193646293707b81547aae0f39698a05c5b4f
Soroborus <soro.borus@mail.ru>
2022-12-06 21:54:49 +0300

    работы 5-10

--- students/21K0586/21K0586-p04/src/main/java/ru/mirea/practice/task2/MenClothing.java
+++
@@ -1,7 +0,0 @@
-package ru.mirea.practice.task2;
-
-public interface MenClothing {
-    default void dressMan() {
-        System.out.println("Одеть мужчину");
-    }
-}
--- students/21K0586/21K0586-p04/src/main/java/ru/mirea/practice/task2/WomenClothing.java
+++
@@ -1,7 +0,0 @@
-package ru.mirea.practice.task2;
-
-public interface WomenClothing {
-    default void dressWomen() {
-        System.out.println("Одеть женщину");
-    }
-}

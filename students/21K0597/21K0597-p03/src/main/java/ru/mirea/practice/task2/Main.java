3f82cd07a82e6ee93e59369d17b71238fb8cd5b2
Egor <egor.golubev2003@gmail.com>
2022-12-07 19:45:40 +0300

    Третья практическая работа

---
+++ students/21K0597/21K0597-p03/src/main/java/ru/mirea/practice/task2/Main.java
@@ -0,0 +1,11 @@
+package ru.mirea.practice.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        Tester a = new Tester();
+        a.toSort();
+        a.max();
+        a.min();
+        a.allCircles();
+    }
+}

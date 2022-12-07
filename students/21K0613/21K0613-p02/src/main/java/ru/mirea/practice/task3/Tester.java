1380263c69c2532e4541a63a22f81516346f30ca
Норден <54271099+nordenfall@users.noreply.github.com>
2022-12-07 04:14:19 +0300

    refactor

--- students/21K0613/21K0613-p02/src/main/java/ru/mirea/practice/task3/Tester.java
+++ students/21K0613/21K0613-p02/src/main/java/ru/mirea/practice/task3/Tester.java
@@ -6,18 +6,18 @@ import java.util.Scanner;
 abstract class Tester {
     private int counter;
 
-    public void main(String[] array) {
+    public void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         try {
             System.out.println("Введите количество окружностей:");
             if (scn.hasNextInt()) {
                 counter = scn.nextInt();
             }
-            Circle[] mass = new Circle[counter];
+            Circle[] array = new Circle[counter];
             for (int i = 0; i < counter; i++) {
                 System.out.println("Введите координаты центр. точки:");
                 Circle circle = new Circle(scn.nextInt(), scn.nextInt());
-                mass[i] = circle;
+                array[i] = circle;
             }
         } catch (InputMismatchException e) {
             System.out.println("Error");

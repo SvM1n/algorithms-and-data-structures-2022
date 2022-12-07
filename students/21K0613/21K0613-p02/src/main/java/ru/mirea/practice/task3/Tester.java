a51b8e40eb9158a56c14cbdba303eb1d5f65e17d
Норден <54271099+nordenfall@users.noreply.github.com>
2022-12-07 04:18:17 +0300

    refactor2

--- students/21K0613/21K0613-p02/src/main/java/ru/mirea/practice/task3/Tester.java
+++ students/21K0613/21K0613-p02/src/main/java/ru/mirea/practice/task3/Tester.java
@@ -4,12 +4,11 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 abstract class Tester {
-    private int counter;
-
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         try {
             System.out.println("Введите количество окружностей:");
+            int counter = 0;
             if (scn.hasNextInt()) {
                 counter = scn.nextInt();
             }

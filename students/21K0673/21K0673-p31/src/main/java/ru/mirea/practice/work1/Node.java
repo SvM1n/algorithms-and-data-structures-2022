0938cc8a9bb4a504bf641da25ba3d246261b38c7
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-12-07 20:37:12 +0300

    31 практика

---
+++ students/21K0673/21K0673-p31/src/main/java/ru/mirea/practice/work1/Node.java
@@ -0,0 +1,13 @@
+package ru.mirea.practice.work1;
+
+public class Node {
+    int key;
+    int height;
+    Node left;
+    Node right;
+
+    Node(int d) {
+        key = d;
+        height = 1;
+    }
+}
